8f8c506c8dfcd7185e151d438a780594f3593829
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-09-30 22:42:31 +0530

    Update great_small.py

--- class_assignments/(17-09-2021) Introduction_to_python/great_small.py
+++ class_assignments/(17-09-2021) Introduction_to_python/great_small.py
@@ -8,7 +8,16 @@
 
 
 n=list(map(int,list(input())))
+
+#Sorting the list 
+#converting every element to string because join method accepts only string object 
 n=list(map(str,sorted(n)))
+
+#joining the sorted elements
 s="".join(n)
+
+#As the sorted method sorts in ascending order we have to reverse the string to get greatest number
 print("Greatest number:",s[::-1])
+
+#joined string is directly printed
 print("Smallest number:",s)

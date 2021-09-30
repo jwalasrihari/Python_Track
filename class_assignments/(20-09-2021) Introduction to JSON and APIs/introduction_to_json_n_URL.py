3af71b90afe895c50e501003db041fab9b27bf4e
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-09-30 23:11:07 +0530

    Update introduction_to_json_n_URL.py

--- class_assignments/(20-09-2021) Introduction to JSON and APIs/introduction_to_json_n_URL.py
+++ class_assignments/(20-09-2021) Introduction to JSON and APIs/introduction_to_json_n_URL.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 20 15:57:38 2021
-
-@author: jwala
-"""
+#PROGRAM       : JSON creation and reading,
+#                Checking whether URL is exists or not and Reading data from URL
+#PROGRAMMED BY : Badam Jwala Sri Hari
+#MAIL ID       : jwalasrihari1330@gmail.com
+#DATE          : 20-09-2021
+#PYTHON VERSION: 3.9.7
+#CAVEATS       : None
+#LICENSE       : None
 
 # PYTHON REQUESTS & API
 
@@ -15,21 +17,24 @@ data={
       }
 
 class jwala_json:
+      
+    # method to creating JSON file and writing data into to it
     def create_json(self, data):
         with open("inserting_data_checking_purpose.json", "w") as file:
             return json.dump(data, file)
-    
+      
+    # method to read data from json file
     def read_json(self, file):
         with open(file)as file:
             return json.load(file)
-    
+    # method to check whether url exists or not
     def check_url(self, url):
         try:
             url = requests.get(url)
             return True
         except:
             return False
-
+    # method to read darta from URLs
     def read_url(self, url):
         url = requests.get(url)
         return url.text

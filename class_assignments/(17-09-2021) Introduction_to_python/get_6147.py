1e72120c773cbbefe024d620eeb821656283317f
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-09-30 22:13:45 +0530

    Update get_6147.py

--- class_assignments/(17-09-2021) Introduction_to_python/get_6147.py
+++ class_assignments/(17-09-2021) Introduction_to_python/get_6147.py
@@ -6,12 +6,12 @@
 # PROGRAMMED BY: B Jwala Sri Hari
 # MAIL ID : jwalasrihari1330@gmail
 # DATE    : 17-09-2021
-# VERSION : 3.7.9
+# VERSION : 3.9.7
 # CAVEATS : None
 # LICENSE : None
 
 
-
+#method to find largest and smallest number possible from digits of given number
 def great_small(p):
     p=list(map(str,sorted(p)))
     s="".join(p)
@@ -21,14 +21,19 @@ def great_small(p):
     return int(s[::-1]),int(s)
 
 n=list(map(int,list(input())))
+
+#store list is used to store difference between largest and smallest
 store=[]
 h,l=great_small(n)
 while(1):
     h,l=great_small(list(str(h-l)))
     temp=h-l
+    
+    # if difference is present in store list then again sequence will repeat, so printing FALSE and exiting from loop
     if temp in store:
         print("FALSE")
         break
+    # if difference is 6174 printing TRUE and exiting from loop
     if temp==6174:
         print("TRUE")
         break

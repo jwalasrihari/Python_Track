50ccf46a238db21850b2845a3594a9b49d59a7ea
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-10-01 00:14:30 +0530

    Update app.py

--- class_assignments/(22-09-2021) Introduction to Flask/app.py
+++ class_assignments/(22-09-2021) Introduction to Flask/app.py
@@ -1,3 +1,13 @@
+#PROGRAM       : Introduction to Flask Framework
+#PROGRAMMED BY : Badam Jwala Sri Hari
+#MAIL ID       : jwalasrihari1330@gmail.com
+#DATE          : 22-09-2021
+#PYTHON VERSION: 3.9.7
+#FLASK VERSION : 2.0.1
+#CAVEATS       : None
+#LICENSE       : None
+
+# Importing the required packages
 from flask import Flask 
 from flask import render_template 
 from flask import redirect 
@@ -5,10 +15,15 @@ from flask import url_for
 
 app = Flask(__name__) 
 
+# Starting page of website
 @app.route('/')
 def index():
+    
+    # Returns html page which will renders on the website
     return render_template('index.html')
 
+
+# Uer page
 @app.route('/user')
 def user():
     return render_template('user.html')

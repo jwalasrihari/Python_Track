f9876c95183f148f7d79c12a65a9e736bf90f281
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-09-30 22:05:46 +0530

    Update common_factors_all_list.py

--- class_assignments/(17-09-2021) Introduction_to_python/common_factors_all_list.py
+++ class_assignments/(17-09-2021) Introduction_to_python/common_factors_all_list.py
@@ -1,12 +1,27 @@
+#PROGRAM       : Find the Common factor of all elements in list
+#PROGRAMMED BY : Badam Jwala Sri Hari
+#MAIL ID       : jwalasrihari1330@gmail.com
+#DATE          : 17-09-2021
+#PYTHON VERSION: 3.9.7
+#CAVEATS       : None
+#LICENSE       : None
+
+
+#method to find GCD of given two numbers
 def gcd(x,y):
     while(y):
         x,y = y,x%y
     return x
+
 lst=list(map(int,input().split()))
 g=gcd(lst[0],lst[1])
+
+#iterating over the list and find the GCD
 for i in range(2,len(lst)):
     g=gcd(g,lst[i])
 
+    
+#factors of GCD of all elements
 for i in range(1,g+1):
     if(g%i==0):
         print(i)

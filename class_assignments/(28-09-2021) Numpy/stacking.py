d61f183509f765199dc3c181fb9ad92b685a1117
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-10-01 10:23:13 +0530

    Update stacking.py

--- class_assignments/(28-09-2021) Numpy/stacking.py
+++ class_assignments/(28-09-2021) Numpy/stacking.py
@@ -1,3 +1,12 @@
+#PROGRAM       : Concatenation on two numpy array vertically and horizantally
+#PROGRAMMED BY : Badam Jwala Sri Hari
+#MAIL ID       : jwalasrihari1330@gmail.com
+#DATE          : 28-09-2021
+#PYTHON VERSION: 3.9.7
+#CAVEATS       : None
+#LICENSE       : None
+
+
 import numpy as np
 array_1 = np.array([(23, 56,89),(67,99,56)])
 array_2 = np.array([(89,25,69),(890,123,5689)])

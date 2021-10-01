1ae6150a88e4e0a91f2bca6dc00a86aaa11842de
jwalasrihari <61618519+jwalasrihari@users.noreply.github.com>
2021-10-01 15:11:39 +0530

    Update data_frame_assignment.py

--- class_assignments/(01-10-2021) Pandas/data_frame_assignment.py
+++ class_assignments/(01-10-2021) Pandas/data_frame_assignment.py
@@ -1,4 +1,11 @@
-
+#PROGRAM       : Read data from URL, make dataframe and store the data in txt file
+#PROGRAMMED BY : Badam Jwala Sri Hari
+#MAIL ID       : jwalasrihari1330@gmail.com
+#DATE          : 01-10-2021
+#PYTHON VERSION: 3.9.7
+#PANDAS VERSION: 1.3.3
+#CAVEATS       : None
+#LICENSE       : None
 
 # PYTHON REQUESTS & API
 
@@ -33,11 +40,18 @@ url = "https://api.thedogapi.com/v1/breeds"
 
 s = jwala()
 
-if s.check_url(url):
-    dic=s.read_url(url)
 
+#checking whether the URL is working or not 
+if s.check_url(url):
+    
+    # Reading data from URL 
+    dic=s.read_url(url)
+    
+    # Converting data into dataFrame
     df=pd.DataFrame(dic)
     print(df)
+    
+    # Storing dataFrame in txt file
     df.to_csv("Dog_breed_pandas.txt", header=None, index=None, sep=' ', mode='a')
 else:
     print("not exists")
